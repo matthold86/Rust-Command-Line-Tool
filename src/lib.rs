ab19ef8dc429612c71414dc6da0cf28d2d3a7b97
Matthew Holden <114833075+matthold86@users.noreply.github.com>
2023-10-30 05:34:41 +0000

    CLI complete

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
-use rusqlite::{Connection, Result};
+use rusqlite::{Connection, Result, params};
+use csv::Reader;
+use std::fs::File;
 
+#[allow(dead_code)]
 #[derive(Debug)]
 pub struct TeamStat {
     pub year: i32,
@@ -28,35 +31,107 @@ pub struct TeamStat {
     pub nc_sos_pyth_rank: i32,
 }
 
-pub fn create_table(conn: &Connection) -> Result<()> {
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS kenpom_stats (
-            year INTEGER,
-            rank INTEGER,
-            team TEXT,
-            conference TEXT,
-            wins INTEGER,
-            losses INTEGER,
-            seed INTEGER,
-            pyth REAL,
-            adjust_o REAL,
-            adjust_o_rank INTEGER,
-            adjust_d REAL,
-            adjust_d_rank INTEGER,
-            adjust_t REAL,
-            adjust_t_rank INTEGER,
-            luck REAL,
-            luck_rank INTEGER,
-            sos_pyth REAL,
-            sos_pyth_rank INTEGER,
-            sos_opp_o REAL,
-            sos_opp_o_rank INTEGER,
-            sos_opp_d REAL,
-            sos_opp_d_rank INTEGER,
-            nc_sos_pyth REAL,
-            nc_sos_pyth_rank INTEGER
-        )",
-        [],
-    )?;
+pub fn create_table(conn: &Connection) -> Result<(), Box<dyn std::error::Error>> {
+    let mut reader: Reader<File> = Reader::from_path("kenpom.csv")?;
+    let (col_names, _) = extract_column_info(&mut reader)?;
+    let query = format!("CREATE TABLE IF NOT EXISTS kenpom_stats ({})", col_names);
+    conn.execute(&query, [])?;
+    Ok(())
+}
+
+pub fn insert_data(conn: &Connection) -> Result<(), Box<dyn std::error::Error>> {
+    // Save iterator as reader
+    let mut reader: Reader<File> = Reader::from_path("kenpom.csv")?;
+    /*let mut reader = match Reader::from_path("kenpom.csv") {
+        Ok(reader) => reader,
+        Err(err) => {
+            println!("Error reading file: {:?}", err);
+            return Ok(());
+        }
+    };*/
+
+    // Retrieve column info
+    let (col_names, num_cols) = extract_column_info(&mut reader)?;
+
+    /*let (col_names, num_cols) = match extract_column_info(&mut reader){
+        Ok((column_names, num_columns)) => (column_names, num_columns),
+        Err(err) => {
+            eprintln!("Error extracting column names: {:?}", err);
+            return Ok(());
+        }
+    };*/
+
+    // Create a list of "?" placeholders with the specified number of columns
+    let placeholders: String = vec!["?".to_string(); num_cols].join(", ");
+
+    // SQL query with the placeholders
+    let query = format!("INSERT INTO kenpom_stats ({}) VALUES ({});", col_names, placeholders);
+    //let mut stmt = conn.prepare(&query);
+    for result in reader.records() {
+        match result {
+            Ok(record) => {
+                let year = &record[0];
+                let rank = &record[1];
+                let team = &record[2];
+                let conference = &record[3];
+                let wins = &record[4];
+                let losses = &record[5];
+                let seed = &record[6];
+                let pyth = &record[7];
+                let adjust_o = &record[8];
+                let adjust_o_rank = &record[9];
+                let adjust_d = &record[10];
+                let adjust_d_rank = &record[11];
+                let adjust_t = &record[12];
+                let adjust_t_rank = &record[13];
+                let luck = &record[14];
+                let luck_rank = &record[15];
+                let sos_pyth = &record[16];
+                let sos_pyth_rank = &record[17];
+                let sos_opp_o = &record[18];
+                let sos_opp_o_rank = &record[19];
+                let sos_opp_d = &record[20];
+                let sos_opp_d_rank = &record[21];
+                let nc_sos_pyth = &record[22];
+                let nc_sos_pyth_rank = &record[23];
+                //let new_params = [year, rank, team, conference, wins, losses, seed, pyth, adjust_o, adjust_o_rank, adjust_d, adjust_d_rank, adjust_t, adjust_t_rank, luck, luck_rank, sos_pyth, sos_pyth_rank, sos_opp_o, sos_opp_o_rank, sos_opp_d, sos_opp_d_rank, nc_sos_pyth, nc_sos_pyth_rank];
+                conn.execute(&query, params![year, rank, team, conference, wins, losses, seed, pyth, adjust_o, adjust_o_rank, adjust_d, adjust_d_rank, adjust_t, adjust_t_rank, luck, luck_rank, sos_pyth, sos_pyth_rank, sos_opp_o, sos_opp_o_rank, sos_opp_d, sos_opp_d_rank, nc_sos_pyth, nc_sos_pyth_rank])?;
+            }
+            Err(err) => {
+                eprintln!("Error reading CSV record: {:?}", err);
+            }
+        }
+    }
+    Ok(())
+}
+
+pub fn extract_column_info(reader: &mut Reader<File>) -> Result <(String, usize), csv::Error> {
+    // Read the header row to get column names
+    let header_record = reader.headers()?.clone();
+
+    // Extract column names and number of columns
+    let column_names: Vec<String> = header_record.iter().map(|name| name.to_string()).collect();
+    let num_columns = column_names.len();
+
+    // Print the column names and number of columns
+    //println!("Column Names: {:?}", column_names);
+    //println!("Number of Columns: {}", num_columns);
+    let column_names = column_names.join(", ");
+    Ok((column_names, num_columns))
+}
+
+pub fn sql_query(conn: &Connection, query: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut stmt = conn.prepare(query)?;
+    let mut rows = stmt.query([])?;
+    let mut count = 0;
+    while let Some(row) = rows.next()? {
+        println!("{:?}", row);
+        count += 1;
+        
+        if count >= 20 {
+            println!("Response limited to 20 lines");
+            break; // Exit the loop after printing 5 rows
+        }
+    }
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,77 @@
 extern crate kenpoms_analysis;
-use kenpoms_analysis::create_table;
+use kenpoms_analysis::{create_table, sql_query};
+use rusqlite::{Connection, Error};
+use std::io;
+use clap::Parser;
 
-fn main(){
-    let conn = rusqlite::Connection::open("kenpom.db").unwrap();
-    create_table(&conn).unwrap();
+#[derive(Parser)]
+#[clap(
+    version = "1.0",
+    author = "Matthew Holden <matthew.holden@duke.edu>",
+    about = "Create a CLI for college basketball statistics database."
+)]
+
+struct CLIQuery {
+    #[clap(short, long)]
+    query: String,
+}
+
+fn establish_connection() -> Result<Connection, Error> {
+    let conn = Connection::open("kenpom.db")?;
+    Ok(conn)
+}
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+
+    let conn: Connection = match establish_connection() {
+        Ok(conn) => {
+            // The connection is established successfully
+            println!("Connected to the KenPoms database.");
+            conn
+        },
+        Err(err) => {
+            // An error occurred when establishing the connection
+            eprintln!("Error opening the database: {:?}", err);
+            return Ok(())
+        }
+    };
+
+
+    create_table(&conn)?;
+
+    //insert_data(&conn)?;
+
+    let mut count = 0;
+
+    while count < 10 {
+
+        count += 1;
+
+        let intro = format!("({} of 10) Enter a SQL Query or type 'Exit': ", count);
+        println!("{}", intro);
+
+        let mut user_input = String::new();
+        io::stdin().read_line(&mut user_input).expect("Failed to read line");
+
+        if user_input == String::from("Exit") {
+            break; // Exit the loop if the user likes the salad
+        }
+
+        let query: String = user_input;
+
+
+        //let query = String::from("SELECT year, team, adjust_o FROM kenpom_stats WHERE team = 'Duke';");
+        match sql_query(&conn, &query) {
+            Ok(_) => continue,
+            Err(err) => {
+                eprintln!("Error {} \n", err);
+                println!("An error occurred with the previous query. Please try again \n");
+                continue;
+            }
+        };
+    }
+
+    println!("Exiting College Basketball Stats CLI")
+    
+    Ok(())
 }
